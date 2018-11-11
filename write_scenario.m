a783ad91939a6255277a1b84a8f8a759eab75058
binghui89 <bli6@ncsu.edu>
2018-11-11 17:30:27 -0600

    Add a scritp to write scenario data to csv.

---
+++ write_scenario.m
@@ -0,0 +1,33 @@
+function write_scenario(sid, xa, xf, xs, header)
+% Write scenario data to csv file with given headers
+% sid: nI by 1, xa and xf dim: nT by nI, xs dim: nT by nI by nS
+% header: cell vector of (nS + 2) by 1, including xa and xf
+dirwork = 'scenario_data';
+dirhome = pwd;
+commaHeader = [header;repmat({','},1,numel(header))]; %insert commaas
+commaHeader = commaHeader(:)';
+textHeader = cell2mat(commaHeader); %cHeader in text with commas
+
+nI = numel(sid);
+nT = size(xa, 1);
+nS = size(xs, 3);
+
+if ~isdir(dirwork)
+    mkdir(dirwork);
+end
+cd(dirwork);
+for i = 1: nI
+    %write header to file
+    fname = strcat(int2str(sid(i)), '.csv');
+    fid = fopen(fname,'w'); 
+    fprintf(fid,'%s\n',textHeader);
+    fclose(fid);
+    
+    %write data to end of file
+    yourdata = nan(nT, nS+2); % Include actual and deterministic forecast
+    yourdata = [xa(:, i), xf(:, i), squeeze(xs(:, i, :))];
+    dlmwrite(fname,yourdata,'-append');
+end
+cd(dirhome);
+
+end
